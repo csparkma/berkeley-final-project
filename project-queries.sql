1f3dc08ceea97cb53aae123e7b523b040aca2b0c
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-10-11 06:34:19 -0400

    sql files for db

---
+++ project-queries.sql
@@ -0,0 +1,6 @@
+--Testing tables queries--
+--Customer table--
+SELECT * FROM customers;
+
+--big query master table--
+SELECT * FROM bigquery_totals;
---
+++ project-schema.sql
@@ -0,0 +1,36 @@
+-- Creating tables for Customers and BigQuery Totals
+CREATE TABLE customers (
+     fullvisitorId VARCHAR(19) NOT NULL,
+     visitId INT NOT NULL,
+);
+
+CREATE TABLE bigquery_totals (
+  	fullvisitorId VARCHAR(19),
+	visitId integer,
+	visitNumber integer,
+	visitStartTime integer,
+	date date,
+	socialEngagementType VARCHAR(50),
+	channelGrouping VARCHAR(15),
+	browser VARCHAR(50),
+	operatingSystem VARCHAR(15),
+	deviceCategory VARCHAR(7),
+	subContinent VARCHAR(25),
+	country VARCHAR(50),
+	region VARCHAR(50),
+	metro VARCHAR(50),
+	city VARCHAR(50),
+	referralPath VARCHAR(255),
+	source VARCHAR(100),
+	medium VARCHAR(10),
+	isTrueDirect boolean,
+	bounces integer,
+	hits integer,
+	newVisits integer,
+	pageviews integer,
+	sessionQualityDim integer,
+	totalTransactionRevenue bigint,
+	transactionRevenue bigint,
+	transactions integer,
+	visits integer,
+);
